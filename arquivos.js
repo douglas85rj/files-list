8dc16d27ba0dc2bca0026da863105065a5fd5c14
douglas85rj <douglas85rj@gmail.com>
2022-10-25 12:57:45 -0300

    gitnore

--- arquivos.js
+++ arquivos.js
@@ -1,31 +1,29 @@
+const fs = require("fs");
+const path = require("path");
 
-const fs = require('fs');
-const path = require('path');
-
-var raiz = './files';
+var raiz = "./files";
 var data = {};
 var aux = 0;
 
-function listaDeArquivos(base){
-    var pastas = fs.readdirSync(base);
+function listaDeArquivos(base) {
+  var pastas = fs.readdirSync(base);
 
-    for (p in pastas){
-        var caminho = path.join(base,pastas[p]);
-        var stats = fs.lstatSync(caminho);
+  for (p in pastas) {
+    var caminho = path.join(base, pastas[p]);
+    var stats = fs.lstatSync(caminho);
 
-        if(stats.isDirectory()){
- listaDeArquivos(caminho);
-        }else{
-data[aux] = {
-    nome: path.basename(caminho),
-    tamanho: stats.size+'bytes',
-    modificação: stats.ctime
+    if (stats.isDirectory()) {
+      listaDeArquivos(caminho);
+    } else {
+      data[aux] = {
+        nome: path.basename(caminho),
+        tamanho: stats.size + "bytes",
+        modificação: stats.ctime,
+      };
 
-};
-
-aux += 1;
-        }
+      aux += 1;
     }
+  }
 }
 
 listaDeArquivos(raiz);
